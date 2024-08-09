0f410496a56b6152e566f5e4f8fbdc52abd44a6d
SJ <100001092+bnbnac@users.noreply.github.com>
2024-08-09 13:10:22 +0900

    init

---
+++ src/main/java/com/letstock/LetstockApplication.java
@@ -0,0 +1,13 @@
+package com.letstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetstockApplication.class, args);
+	}
+
+}
