c45bdfbde43db3ea5b2796965804497a7060e1d0
SJ <100001092+bnbnac@users.noreply.github.com>
2024-08-15 15:02:46 +0900

    feat: 포스트 작성 기능

---
+++ src/main/java/com/letstock/service/post/dto/request/PostCreate.java
@@ -0,0 +1,22 @@
+package com.letstock.service.post.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostCreate {
+
+    @NotBlank(message = "타이틀을 입력해주세요")
+    private String title;
+
+    @NotBlank(message = "콘텐츠를 입력해주세요")
+    private String content;
+
+    @Builder
+    public PostCreate(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
