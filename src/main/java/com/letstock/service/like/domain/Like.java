8fa19be1736b53eed0c1c618bb9e0c53954c567f
SJ <100001092+bnbnac@users.noreply.github.com>
2024-08-16 20:30:19 +0900

    feat: Like 기능 - 이미 Like 한 target을 다시 Like 할 수 없다 - activity type을 재사용하기보다는 like type을 만들어 적은 비용으로 구분을 명확히 함 - Like 객체는 자신의 like type에 따라 activity type을 생성하는 메소드를 갖는다

---
+++ src/main/java/com/letstock/service/like/domain/Like.java
@@ -0,0 +1,52 @@
+package com.letstock.service.like.domain;
+
+
+import com.letstock.service.feed.domain.ActivityType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "likes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdAt;
+
+    private LikeType type;
+
+    private Long targetId;
+
+    private Long memberId;
+
+    @Builder
+    public Like(LikeType type, Long targetId, Long memberId) {
+        Assert.notNull(type, "type must not be null");
+        Assert.notNull(targetId, "targetId must not be null");
+        Assert.notNull(memberId, "memberId must not be null");
+
+        this.createdAt = LocalDateTime.now();
+        this.type = type;
+        this.targetId = targetId;
+        this.memberId = memberId;
+    }
+
+    public ActivityType generateActivityType() {
+        if (type == LikeType.POST) {
+            return ActivityType.POST_LIKE;
+        }
+
+        return ActivityType.COMMENT_LIKE;
+    }
+
+}
